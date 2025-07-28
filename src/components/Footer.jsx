87a93662f7e4a307f03b3eb0b7e33c41002fe285
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-07-28 15:50:02 +0200

    .

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+ 
+  
+import { Link } from "react-router-dom";
+  
+  const Footer = () => {
+    return (
+      <div>
+          
+  <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+  <nav>
+    <h6 className="footer-title">Services</h6>
+       <Link className="link link-hover" to="/destinations">
+          Destinations
+        </Link>
+  </nav>
+
+  <nav>
+    <h6 className="footer-title">Company</h6>
+     <Link className="link link-hover" to="/about">
+          About
+        </Link>
+      <Link className="link link-hover" to="/contact">
+          Contact
+        </Link>
+  </nav>
+
+  <nav>
+    <h6 className="footer-title">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+
+      </div>
+    )
+  }
+  
+  export default Footer
+  
